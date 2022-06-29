bed40867de8fc436a725ad0a0667f9691aa3712f
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-29 12:48:14 -0700

    first bird route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,11 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('getAll birds', async () => {
+    const resp = await request(app).get('/api/v1/birds');
+
+    expect(resp.status).toBe(200);
+    expect(resp.body[0].id).toBe('1');
   });
   afterAll(() => {
     pool.end();
