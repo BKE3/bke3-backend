4d039298d550ffa121efaf18b6bb8532e88ed7c4
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-29 14:54:55 -0700

    wrote test for getBirdById

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,13 @@ describe('backend-express-template routes', () => {
     expect(resp.status).toBe(200);
     expect(resp.body[0].id).toBe('1');
   });
+
+  it('getBirdById should return one bird with all the detail', async () => {
+    const resp = await request(app).get('/api/v1/birds/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body.id).toBe('1');
+    expect(resp.body.latin_name).toBe('Cyanocitta');
+  });
   afterAll(() => {
     pool.end();
   });
