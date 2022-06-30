a3905acf81afaa8ee9f7bf0626ce1ba9cdec4584
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-30 14:07:15 -0700

    wrote test testing put

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -36,6 +36,18 @@ describe('backend-express-template routes', () => {
     expect(resp.status).toEqual(200);
     expect(resp.body.latin_name).toEqual('Psittaciformes');
   });
+  it('updateBird should update a bird', async () => {
+    const resp = await request(app).put('/api/v1/birds/1').send({
+      latin_name: 'Psittaciformes',
+      common_name: 'Parrot',
+      habitat: 'woodlands, rainforests, palm forests, savannas, grasslands, desert edges, scrubland',
+      life_expectancy: '40 to 70 years',
+      flightless: false,
+      image_url: 'https://cdn.shopify.com/s/files/1/1111/3280/files/parrot-hero-slide_5f608557-2722-4363-964c-856af23751ad_600x600.jpg?v=1608776007',
+    });
+    expect(resp.status).toEqual(200);
+    expect(resp.body.latin_name).toEqual('Psittaciformes');
+  });
   afterAll(() => {
     pool.end();
   });
