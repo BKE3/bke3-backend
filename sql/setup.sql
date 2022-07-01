b8c66be944983284b80cb00e8b8ab12367746f38
Beth <bethmelmtv@gmail.com>
2022-06-30 18:05:44 -0700

    worked on fixing create user bug, bug is fixed

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS birds;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE birds (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -24,4 +25,11 @@ VALUES
 ('Cyanocitta','Blue Jay', 'Oak and pin woods, suburban gardens, groves, towns.', '7 years', false, 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Blue_jay_in_PP_%2830960%29.jpg/440px-Blue_jay_in_PP_%2830960%29.jpg'),
 ('Strigiformes', 'Owl', 'coniferous forests, mountains, deserts, and plains', '5-12 years in the wild', false, 'https://cdn.britannica.com/86/34386-050-25B31B35/Great-horned-owl.jpg'),
 ('Oxyura jamaicensis','Ruddy Duck', 'Fresh marshes, ponds, lakes; in winter, salt bays.', '13 years', true, 'https://www.allaboutbirds.org/guide/assets/photo/302124271-480px.jpg
-')
+');
+
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+)
